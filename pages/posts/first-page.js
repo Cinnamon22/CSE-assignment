c79659045fcc5201a5c1d4775d83dcddc22bf8e3
Cinnamon22 <ryh1517@naver.com>
2022-05-10 19:53:30 +0900

    new file

---
+++ pages/posts/first-page.js
@@ -0,0 +1,22 @@
+import Link from 'next/link'
+
+
+export default function FirstPost() {
+  return (
+  <>
+    <h1>My name : Cinnamon
+    </h1>
+    <h2>
+    my hobby : sleeping
+    </h2>
+<h3>
+anything : I like CSE course.
+</h3>
+
+    <h4>
+      <Link href="/">
+        <a>Back to home</a>
+      </Link>
+    </h4>
+  </>
+)}
